35452026e1981b79a7cbdf4020d6e1c23f247d6b
Somies <misaelsoria789@gmail.com>
2024-12-23 17:44:26 -0500

    Descripción breve de los cambios realizados

--- src/portada.jsx
+++ src/portada.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import './cabecera.css';
 import './portada.css';
+import './pie.css';
 
 const Portada = () => {
   const [menuVisible, setMenuVisible] = useState(false);
+  const [mostrarContenido, setMostrarContenido] = useState(false);
 
   const toggleMenu = () => {
     setMenuVisible(!menuVisible);
+  };
+  const toggleContenido = () => {
+    setMostrarContenido(!mostrarContenido);
   };
 
   return (
@@ -60,7 +65,7 @@ const Portada = () => {
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
                 >
-                  Programación
+                  SoftEd
                 </a>
                 <ul className="dropdown-menu">
                   <a className="dropdown-item" href="#">Front-end</a>
@@ -86,6 +91,75 @@ const Portada = () => {
           </section>
         </section>
       </main>
+
+
+      <footer>
+    <section className="pie">
+        <section className="sub1pie">
+            {/* Información general */}
+            <div className="info">
+                <h3>Sobre Nosotros</h3>
+                <p>
+                    Enseñamos cursos gratuitos para inicial, primaria, secundaria, inglés y programación (frontend-backend).
+                </p>
+                {/* Texto adicional */}
+                <p className={mostrarContenido ? "mostrar" : "ocultar"}>
+                    {/* Enlaces rápidos */}
+                <div className='informacion'>
+                    <div className="enlaces">
+                        <h3>Categorías</h3>
+                        <ul>
+                            <li><a href="#inicial">Inicial</a></li>
+                            <li><a href="#primaria">Primaria</a></li>
+                            <li><a href="#secundaria">Secundaria</a></li>
+                            <li><a href="#ingles">Inglés</a></li>
+                            <li><a href="#programacion">Programación</a></li>
+                        </ul>
+                    </div>
+
+                    {/* Redes sociales */}
+                    <div className="redes">
+                        <h3>Síguenos</h3>
+                        <div className="icons">
+                            <a href="#"><img src="../facebook.png" alt="Facebook" /></a>
+                            <a href="#"><img src="twiter.png" alt="Twitter" /></a>
+                            <a href="#"><img src="instagram.png" alt="Instagram" /></a>
+                            <a href="#"><img src="wasap.png" alt="YouTube" /></a>
+                        </div>
+                    </div>
+                    </div>
+                </p>
+                {/* Flecha para mostrar/ocultar el contenido */}
+                <span className="flecha" onClick={toggleContenido}>
+                    {mostrarContenido ? "▲" : "▼"}
+                </span>
+            </div>
+            <div className="enlaces">
+                        <h3>Categorías</h3>
+                        <ul>
+                            <li><a href="#inicial">Inicial</a></li>
+                            <li><a href="#primaria">Primaria</a></li>
+                            <li><a href="#secundaria">Secundaria</a></li>
+                            <li><a href="#ingles">Inglés</a></li>
+                            <li><a href="#programacion">Programación</a></li>
+                        </ul>
+                    </div>
+
+                    {/* Redes sociales */}
+                    <div className="redes">
+                        <h3>Síguenos</h3>
+                        <div className="icons">
+                            <a href="#"><img src="../facebook.png" alt="Facebook" /></a>
+                            <a href="#"><img src="twiter.png" alt="Twitter" /></a>
+                            <a href="#"><img src="instagram.png" alt="Instagram" /></a>
+                            <a href="#"><img src="wasap.png" alt="YouTube" /></a>
+                        </div>
+                    </div>
+        </section>
+    </section>
+    
+</footer>
+
 
 
 
